d1a47a6821692fb829c25b1ba96013cb9dd40df8
itssnehalhere <140611068+itssnehalhere@users.noreply.github.com>
2025-03-24 12:36:32 +0530

    Update Test5Application.java

--- src/main/java/com/test5/test5/Test5Application.java
+++ src/main/java/com/test5/test5/Test5Application.java
@@ -7,6 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test5Application {
 
 	public static void main(String[] args) {
+		String name="mike";
+		int x=100;
+		String s="natalia";
+                boolean y=true;
+                int z=50;
+		boolean z=false;
+		float z=0.3f;
 		SpringApplication.run(Test5Application.class, args);
 	}
 
